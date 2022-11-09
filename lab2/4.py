6c694837bb3ea709b8a0cd5447a9bbdd365761ab
alex5kov1 <al_5@abv.bg>
2022-11-09 21:19:12 +0200

    4.py

--- lab2/4.py
+++ lab2/4.py
@@ -1,6 +1,6 @@
 num = int(input("Enter number:"))
 bit = int(input("Enter bit:"))
 if num & (1 << bit) > 0:
-    print("False")
-else:
     print("True")
+else:
+    print("False")
--- lab2/5.py
+++ lab2/5.py
@@ -1,3 +1,17 @@
-a = 27102022
-print(bin(a))
-print(hex(a))
+import string
+symbols = string.digits + string.ascii_uppercase
+number = 27102022
+binary_num = ""
+hex_num = ""
+
+while number != 0:
+    binary_num += str(number % 2)
+    number //= 2
+
+number = 27102022
+while number != 0:
+    hex_num += symbols[number % 16]
+    number //= 16
+
+print(binary_num[::-1])
+print(hex_num[::-1])
